ae5b9635924a38cb70a5f53a13042466489907ef
idanbakal <idan19982@hotmail.co.il>
2023-05-08 17:47:31 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+console.log(`hey men`);
+let arr = [1, 2, 3, 4, 5, 6];
+let com = `hey`;
+function is() {
+    document.getElementById('#home').style.backgroundColor = "brown";
+}
+const home = document.getElementById(`#home`);
+home.textContent = `HAHAHA I AM THE KING`;
+//# sourceMappingURL=index.js.map
